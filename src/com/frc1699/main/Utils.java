bb6475ee3560fa1006ee25fdd4088ae5cf2f45dc
Jakob Misbach <jakobmisbach8@gmail.com>
2018-04-19 17:16:43 -0400

    Filled out one request

--- src/com/frc1699/main/Utils.java
+++ src/com/frc1699/main/Utils.java
@@ -1,5 +1,7 @@
 package com.frc1699.main;
 
+import com.frc1699.team.Team;
+
 public class Utils {
 
     public static Object makeRequest(final String reqURL) throws InvalidURLException {
@@ -10,27 +12,28 @@ public class Utils {
         return null;
     }
 
-    public static String buildRequestURL(final RequestType reqType, final String teamNumber){
+    public static String buildRequestURL(final RequestType reqType, final Team team){
         switch(reqType){
             case MatchReq:
-                return makeMatchReq();
+                return makeMatchReq(team);
             case EventListReq:
-                return makeEventListReq();
+                return makeEventListReq(team);
             case MatchListReq:
-                return makeMatchListReq();
+                return makeMatchListReq(team);
         }
         return "Bad Req";
     }
 
-    private static String makeMatchListReq(){
+    private static String makeMatchListReq(final Team team){
         return null;
     }
 
-    private static String makeEventListReq(){
-        return null;
+    private static String makeEventListReq(final Team team){
+        String outputString = "https://www.thebluealliance.com/api/v3/team/" + team.getTBARequestID() + "/2018/keys";
+        return outputString;
     }
 
-    private static String makeMatchReq(){
+    private static String makeMatchReq(final Team team){
         return null;
     }
 }
