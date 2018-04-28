ca4d6b57e30de42fddfea5e2378ee90c084db66d
squirlemaster42 <squirlemaster@gmail.com>
2018-04-28 15:14:06 -0400

    More Stuff

--- src/com/frc1699/tst/MatchParserTest.java
+++ src/com/frc1699/tst/MatchParserTest.java
@@ -1,15 +1,23 @@
 package com.frc1699.tst;
 
+import com.frc1699.main.Constants;
 import com.frc1699.main.Utils;
 import com.frc1699.parser.Parser;
 import com.frc1699.match.Team;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MatchParserTest {
 
     public static void main(String[] args){
+        Scanner io = new Scanner(System.in);
+        System.out.println("Please enter a TBA auth key: ");
+        String TBAAuthKey = io.nextLine();
+        System.out.println("Your auth key is: " + TBAAuthKey);
+        Constants.getInstance().setTBAAuthKey(TBAAuthKey);
+
         Team team = new Team("254");
         try {
             Object output = Utils.makeRequest(Utils.makeEventListReq(team));
