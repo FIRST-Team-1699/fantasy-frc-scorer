58da451e12b2807252d5fb1aec9118f8722ab82a
Jakob Misbach <jakobmisbach8@gmail.com>
2022-04-21 16:11:54 -0400

    Added more data output

--- src/com/frc1699/main/Main.java
+++ src/com/frc1699/main/Main.java
@@ -12,8 +12,10 @@ public class Main {
             String[] lines = Parser.readPickFile("testData/worlds2022draft.csv");
             Player[] players = Parser.parsePlayers(lines);
 
+            int place = 1;
             for (Player p : Utils.sortPlayers(players))  {
-                System.out.printf("%18s -- %.1f -- %d matches played\n", p.getName(), p.sumQualScore(), p.sumMatchesPlayed());
+                System.out.printf("%d -- %18s -- %.1f points scored -- %d matches played -- %.4f avg points per match\n", place, p.getName(), p.sumQualScore(), p.sumMatchesPlayed(), p.sumQualScore() / (double) p.sumMatchesPlayed());
+                place++;
             }
             System.exit(0);
         } catch (FileNotFoundException e) {
